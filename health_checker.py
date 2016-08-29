b713a3139040c903654a9eb3a4a94b3038bd294e
Anne Ustach <aeu@signalfx.com>
2016-08-29 11:10:54 -0700

    Clean things up and address code review comments

--- health_checker.py
+++ health_checker.py
@@ -4,12 +4,8 @@ import requests
 
 try:
     import collectd
-
 except ImportError:
-    try:
-        import dummy_collectd as collectd
-    except:
-        pass
+    import dummy_collectd as collectd
 
 PLUGIN_NAME = 'health_checker'
 SEND = True
@@ -18,12 +14,6 @@ TYPE = 'gauge'
 
 
 def log(param):
-    """
-    Log messages to either collectd or stdout depending on how it was called.
-    :param param: the message
-    :return:  None
-    """
-
     if __name__ != '__main__':
         collectd.info("%s: %s" % (PLUGIN_NAME, param))
     else:
@@ -31,90 +21,43 @@ def log(param):
 
 
 def config(conf):
-    """
-    Parses and loads options from the collectd plugin config file
-
-    :param conf: a Config object
-    :return: None
-    """
-
     for val in conf.children:
-        if val.key == 'URL':
-            global URL
-            URL = val.values[0]
+        if val.key == 'HEALTH_URL':
+            global HEALTH_URL
+            HEALTH_URL = val.values[0]
         if val.key == 'Instance':
             global INSTANCE
             INSTANCE = val.values[0]
 
 
-def _get_http_response(URL):
-    response = 0
+def _get_http_response(HEALTH_URL):
     try:
-        r = requests.get(URL)
-        response = r.status_code
+        return requests.get(HEALTH_URL).status_code
     except:
-        pass
-    return response
+        return 0
 
 
 def read():
-
-    val = _get_http_response(URL)
+    val = _get_http_response(HEALTH_URL)
     collectd.Values(plugin=PLUGIN_NAME,
                     type_instance="service.health",
                     plugin_instance=INSTANCE,
                     type=TYPE,
                     values=[val]).dispatch()
 
-    global SEND
-    if SEND:
-        notif = collectd.Notification(plugin=PLUGIN_NAME,
-                                      type_instance="started",
-                                      type="objects")
-        notif.severity = 4  # OKAY
-        notif.message = "The %s plugin has just started" % PLUGIN_NAME
-        notif.dispatch()
-        SEND = False
-
 
 def init():
-    """
-    This method has been registered as the init callback; this gives the plugin
-    a way to do startup actions.  We'll just log a message
-    :return: None
-    """
-
     log("Plugin %s initializing..." % PLUGIN_NAME)
 
 
 def shutdown():
-    """
-    This method has been registered as the shutdown callback. this gives the
-    plugin a way to clean up after itself before shutting down.  We'll just log
-    a message.
-    :return: None
-    """
-
     log("Plugin %s shutting down..." % PLUGIN_NAME)
 
 
-def log_cb(severity, message):
-    """
-    This method has been registered as the log callback.
-    :param severity: an integer and small for important messages
-      and high for less important messages
-    :param message: a string without a newline at the end
-    :return: None
-    """
-
-    pass
-
-
-if __name__ != "__main__":
+if __name__ != '__main__':
     # when running inside plugin register each callback
     collectd.register_init(init)
     collectd.register_config(config)
-    collectd.register_log(log_cb)
     collectd.register_read(read)
     collectd.register_shutdown(shutdown)
 else:
